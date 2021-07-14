e6bf04440e60a1f267a9fed44cf491f6e8434338
George Oblapenko <kunstmord@kunstmord.com>
2021-07-14 15:02:15 +0300

    Fixed max level computation

--- src/utils.jl
+++ src/utils.jl
@@ -9,7 +9,7 @@ function compute_densities(p, T::Float64, molar_fractions::Array{Float64,1})
 end
 
 function compute_max_vibr_level(mol::Molecule, T, Tv)
-    maxlevel = round(UInt16, (mol.vibrational_energy[2] / (2 * mol.anharmonic_ratio * constants.h * mol.frequency)) * (T / Tv) + 0.5)
+    maxlevel = round(UInt16, (mol.vibrational_energy[2] / (2 * mol.anharmonic_ratio * constants.h * mol.frequency * constants.c)) * (T / Tv) + 0.5)
 
     if maxlevel > mol.n_vibr
         maxlevel = mol.n_vibr
