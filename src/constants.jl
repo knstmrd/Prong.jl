3e121f1c041cfd9c69956f2f56143a925ba50031
George Oblapenko <kunstmord@kunstmord.com>
2021-07-14 13:11:33 +0300

    Speed of light

--- src/constants.jl
+++ src/constants.jl
@@ -1,6 +1,7 @@
 struct Constants
     k::Float64
     h::Float64
+    c::Float64
 end
 
-const constants = Constants(1.38064852e-23, 6.62607015e-34)
+const constants = Constants(1.38064852e-23, 6.62607015e-34, 2.99792458e8)
