07f0b5dd5f211f7b8120e073b09fd4eb1a3fabd3
George Oblapenko <kunstmord@kunstmord.com>
2021-07-21 19:31:44 +0300

    N_A and R added

--- src/constants.jl
+++ src/constants.jl
@@ -2,6 +2,8 @@ struct Constants
     k::Float64
     h::Float64
     c::Float64
+    N_A::Float64
+    R::Float64
 end
 
-const constants = Constants(1.38064852e-23, 6.62607015e-34, 2.99792458e8)
+const constants = Constants(1.38064852e-23, 6.62607015e-34, 2.99792458e8, 6.02214076e23, 8.31446261815324)
