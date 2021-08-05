7a236affbce1474753a3e9efc7e3dddc9af671a9
George Oblapenko <kunstmord@kunstmord.com>
2021-08-05 14:51:15 +0300

    Tests for vibrational distribution struct and Mixture struct

--- tests/tests.jl
+++ tests/tests.jl
@@ -1,11 +1,13 @@
 using YAML: length
-include("../src/thermodynamic_properties.jl")
+include("../src/mixture.jl")
 using Test
+
+global const molnames = ["N2", "O2", "NO"]
 
 @testset "data loading" begin
     rtol = 0.0001
 
-    for molname in ["N2", "O2", "NO"]
+    for molname in molnames
         mol = create_molecule("data/particles.yaml", molname, simplified_anharmonic=true)
         @test last(mol.vibrational_energy) < mol.dissociation_energy
         @test mol.n_vibr - 1 == UInt16(mol.vibrational_levels[length(mol.vibrational_levels)])
@@ -25,14 +27,15 @@ end
 @testset "distributions" begin
     rtol = 0.0001
 
-    for molname in ["N2", "O2", "NO"]
+    for molname in molnames
         mol = create_molecule("data/particles.yaml", molname, anharmonic=false)
 
+        vd = create_vibrational_distribution(false, false, false)
         for T in [500.0, 5000.0, 20000.0]
             for Tv in [500.0, 5000.0, 20000.0]
-                Zv = compute_Z_vibr(mol, T, Tv)
+                Zv = compute_Z_vibr(mol, vd, T, Tv)
 
-                xi_arr = compute_xi_vibr(mol, T, Tv, Zv)
+                xi_arr = compute_xi_vibr(mol, vd, T, Tv, Zv)
 
                 @test length(xi_arr) == mol.n_vibr
                 @test true == isapprox(sum(xi_arr), 1.0, rtol=rtol)
@@ -41,49 +44,27 @@ end
         end
     end
 
-    for molname in ["N2", "O2", "NO"]
-        mol = create_molecule("data/particles.yaml", molname, simplified_anharmonic=true, use_Treanor=false)
+    for molname in molnames
+        mol = create_molecule("data/particles.yaml", molname, anharmonic=true, simplified_anharmonic=true)
+        vd_boltzmann = create_vibrational_distribution(true, false, false)
+        vd_tr = create_vibrational_distribution(true, true, false)
+        vd_tr_cont = create_vibrational_distribution(true, true, true)
 
-        for T in [500.0, 5000.0, 20000.0]
-            for Tv in [500.0, 5000.0, 20000.0]
-                Zv = compute_Z_vibr(mol, T, Tv)
+        for vd in [vd_boltzmann, vd_tr, vd_tr_cont]
+            for T in [500.0, 5000.0, 20000.0]
+                for Tv in [500.0, 5000.0, 20000.0]
+                    Zv = compute_Z_vibr(mol, vd, T, Tv)
 
-                xi_arr = compute_xi_vibr(mol, T, Tv, Zv)
+                    xi_arr = compute_xi_vibr(mol, vd, T, Tv, Zv)
 
-                @test length(xi_arr) == mol.n_vibr
-                @test true == isapprox(sum(xi_arr), 1.0, rtol=rtol)
-                @test true == all(xi_arr[1:length(xi_arr) - 1] .>= xi_arr[2:length(xi_arr)])
-            end
-        end
-    end
-
-    for molname in ["N2", "O2", "NO"]
-        mol = create_molecule("data/particles.yaml", molname, simplified_anharmonic=true, use_Treanor=true, continue_Treanor_with_Boltzmann=false)
-
-        for T in [500.0, 5000.0, 20000.0]
-            for Tv in [500.0, 5000.0, 20000.0]
-                Zv = compute_Z_vibr(mol, T, Tv)
-
-                xi_arr = compute_xi_vibr(mol, T, Tv, Zv)
-
-                @test true == isapprox(sum(xi_arr), 1.0, rtol=rtol)
-                @test true == all(xi_arr[1:length(xi_arr) - 1] .>= xi_arr[2:length(xi_arr)])
-            end
-        end
-    end
-
-    for molname in ["N2", "O2", "NO"]
-        mol = create_molecule("data/particles.yaml", molname, simplified_anharmonic=true, use_Treanor=true, continue_Treanor_with_Boltzmann=true)
-
-        for T in [500.0, 5000.0, 20000.0]
-            for Tv in [500.0, 5000.0, 20000.0]
-                Zv = compute_Z_vibr(mol, T, Tv)
-
-                xi_arr = compute_xi_vibr(mol, T, Tv, Zv)
-
-                @test length(xi_arr) == mol.n_vibr
-                @test true == isapprox(sum(xi_arr), 1.0, rtol=rtol)
-                @test true == all(xi_arr[1:length(xi_arr) - 1] .>= xi_arr[2:length(xi_arr)])
+                    if vd != vd_tr
+                        @test length(xi_arr) == mol.n_vibr
+                    else
+                        @test length(xi_arr) <= mol.n_vibr
+                    end
+                    @test true == isapprox(sum(xi_arr), 1.0, rtol=rtol)
+                    @test true == all(xi_arr[1:length(xi_arr) - 1] .>= xi_arr[2:length(xi_arr)])
+                end
             end
         end
     end
@@ -96,8 +77,8 @@ end
     rtol = 0.01
     ΔT = 1
 
-    for molname in ["N2", "O2", "NO"]
-        mol = create_molecule("data/particles.yaml", molname)
+    for molname in molnames
+        mol = create_molecule("data/particles.yaml", molname, anharmonic=true, simplified_anharmonic=true)
         for T in [500.0, 1000.0, 2500.0, 5000.0]
             Zr = compute_Z_rot(mol, T)
             Er = compute_E_rot(mol, T, Zr)
@@ -129,32 +110,33 @@ end
         end
     end
 
-    for molname in ["N2", "O2", "NO"]
+    for molname in molnames
         mol = create_molecule("data/particles.yaml", molname, anharmonic=false)
+
+        vd = create_vibrational_distribution(false, false, false)
+
         for T in [500.0, 5000.0, 20000.0]
             for Tv in [500.0, 5000.0, 20000.0]
-                Zv = compute_Z_vibr(mol, T, Tv)
-                Ev = compute_E_vibr(mol, T, Tv, Zv)
-                c_vibrT = compute_c_vibrT(mol, T, Tv, Zv, Ev)
-                c_vibrTv = compute_c_vibrTv(mol, T, Tv, Zv, Ev)
+                Zv = compute_Z_vibr(mol, vd, T, Tv)
+                Ev = compute_E_vibr(mol, vd, T, Tv, Zv)
+                c_vibrT = compute_c_vibrT(mol, vd, T, Tv, Zv, Ev)
+                c_vibrTv = compute_c_vibrTv(mol, vd, T, Tv, Zv, Ev)
 
 
-                Zvm = compute_Z_vibr(mol, T - ΔT, Tv)
-                Evm = compute_E_vibr(mol, T - ΔT, Tv, Zvm) / mol.mass
-                Zvp = compute_Z_vibr(mol, T + ΔT, Tv)
-                Evp = compute_E_vibr(mol, T + ΔT, Tv, Zvp) / mol.mass
+                Zvm = compute_Z_vibr(mol, vd, T - ΔT, Tv)
+                Evm = compute_E_vibr(mol, vd, T - ΔT, Tv, Zvm) / mol.mass
+                Zvp = compute_Z_vibr(mol, vd, T + ΔT, Tv)
+                Evp = compute_E_vibr(mol, vd, T + ΔT, Tv, Zvp) / mol.mass
 
-                # println(molname, "; T=", T, "; Tv=", Tv, ": ", c_vibrT, ", ", (Evp - Evm) / (2 * ΔT))
                 @test c_vibrT <= 0.0
                 @test true == isapprox(c_vibrT, (Evp - Evm) / (2 * ΔT), rtol=rtol)
 
 
-                Zvm = compute_Z_vibr(mol, T, Tv - ΔT)
-                Evm = compute_E_vibr(mol, T, Tv - ΔT, Zvm) / mol.mass
-                Zvp = compute_Z_vibr(mol, T, Tv + ΔT)
-                Evp = compute_E_vibr(mol, T, Tv + ΔT, Zvp) / mol.mass
+                Zvm = compute_Z_vibr(mol, vd, T, Tv - ΔT)
+                Evm = compute_E_vibr(mol, vd, T, Tv - ΔT, Zvm) / mol.mass
+                Zvp = compute_Z_vibr(mol, vd, T, Tv + ΔT)
+                Evp = compute_E_vibr(mol, vd, T, Tv + ΔT, Zvp) / mol.mass
 
-                # println(molname, "; T=", T, "; Tv=", Tv, ": ", c_vibrTv, ", ", (Evp - Evm) / (2 * ΔT))
                 @test true == isapprox(c_vibrTv, (Evp - Evm) / (2 * ΔT), rtol=rtol)
             end
         end
@@ -162,21 +144,24 @@ end
 
 
     # ΔT = 0.05
-    for molname in ["N2", "O2", "NO"]
+    for molname in molnames
+        mol = create_molecule("data/particles.yaml", molname, anharmonic=true, simplified_anharmonic=true)
+
         for continue_Treanor_with_Boltzmann in [true, false]
-            mol = create_molecule("data/particles.yaml", molname, anharmonic=true, simplified_anharmonic=true, continue_Treanor_with_Boltzmann=continue_Treanor_with_Boltzmann)
+            # continue_Treanor_with_Boltzmann=continue_Treanor_with_Boltzmann)
+            vd = create_vibrational_distribution(true, true, continue_Treanor_with_Boltzmann)
             for T in [500.0, 5000.0, 20000.0]
                 for Tv in [500.0, 5000.0, 20000.0]
-                    Zv = compute_Z_vibr(mol, T, Tv)
-                    Ev = compute_E_vibr(mol, T, Tv, Zv)
-                    c_vibrT = compute_c_vibrT(mol, T, Tv, Zv, Ev)
-                    c_vibrTv = compute_c_vibrTv(mol, T, Tv, Zv, Ev)
+                    Zv = compute_Z_vibr(mol, vd, T, Tv)
+                    Ev = compute_E_vibr(mol, vd, T, Tv, Zv)
+                    c_vibrT = compute_c_vibrT(mol, vd, T, Tv, Zv, Ev)
+                    c_vibrTv = compute_c_vibrTv(mol, vd, T, Tv, Zv, Ev)
 
 
-                    Zvm = compute_Z_vibr(mol, T - ΔT, Tv)
-                    Evm = compute_E_vibr(mol, T - ΔT, Tv, Zvm) / mol.mass
-                    Zvp = compute_Z_vibr(mol, T + ΔT, Tv)
-                    Evp = compute_E_vibr(mol, T + ΔT, Tv, Zvp) / mol.mass
+                    Zvm = compute_Z_vibr(mol, vd, T - ΔT, Tv)
+                    Evm = compute_E_vibr(mol, vd, T - ΔT, Tv, Zvm) / mol.mass
+                    Zvp = compute_Z_vibr(mol, vd, T + ΔT, Tv)
+                    Evp = compute_E_vibr(mol, vd, T + ΔT, Tv, Zvp) / mol.mass
 
                     # some numerical precision issues arise for this distribution, so we relax the tolerances a bit
                     if (abs(c_vibrT) > 1e-8)
@@ -185,41 +170,37 @@ end
                         @test true == isapprox(c_vibrT, (Evp - Evm) / (2 * ΔT), atol=1e-10)
                     end
 
-                    Zvm = compute_Z_vibr(mol, T, Tv - ΔT)
-                    Evm = compute_E_vibr(mol, T, Tv - ΔT, Zvm) / mol.mass
-                    Zvp = compute_Z_vibr(mol, T, Tv + ΔT)
-                    Evp = compute_E_vibr(mol, T, Tv + ΔT, Zvp) / mol.mass
+                    Zvm = compute_Z_vibr(mol, vd, T, Tv - ΔT)
+                    Evm = compute_E_vibr(mol, vd, T, Tv - ΔT, Zvm) / mol.mass
+                    Zvp = compute_Z_vibr(mol, vd, T, Tv + ΔT)
+                    Evp = compute_E_vibr(mol, vd, T, Tv + ΔT, Zvp) / mol.mass
 
                     @test true == isapprox(c_vibrTv, (Evp - Evm) / (2 * ΔT), rtol=rtol)
                 end
             end
         end
-    end
 
-
-    # ΔT = 1
-    for molname in ["N2", "O2", "NO"]
-        mol = create_molecule("data/particles.yaml", molname, anharmonic=true, simplified_anharmonic=true, use_Treanor=false)
+        vd = create_vibrational_distribution(true, false, false)
         for T in [500.0, 5000.0, 20000.0]
             for Tv in [500.0, 5000.0, 20000.0]
-                Zv = compute_Z_vibr(mol, T, Tv)
-                Ev = compute_E_vibr(mol, T, Tv, Zv)
-                c_vibrT = compute_c_vibrT(mol, T, Tv, Zv, Ev)
-                c_vibrTv = compute_c_vibrTv(mol, T, Tv, Zv, Ev)
+                Zv = compute_Z_vibr(mol, vd, T, Tv)
+                Ev = compute_E_vibr(mol, vd, T, Tv, Zv)
+                c_vibrT = compute_c_vibrT(mol, vd, T, Tv, Zv, Ev)
+                c_vibrTv = compute_c_vibrTv(mol, vd, T, Tv, Zv, Ev)
 
 
-                Zvm = compute_Z_vibr(mol, T - ΔT, Tv)
-                Evm = compute_E_vibr(mol, T - ΔT, Tv, Zvm) / mol.mass
-                Zvp = compute_Z_vibr(mol, T + ΔT, Tv)
-                Evp = compute_E_vibr(mol, T + ΔT, Tv, Zvp) / mol.mass
+                Zvm = compute_Z_vibr(mol, vd, T - ΔT, Tv)
+                Evm = compute_E_vibr(mol, vd, T - ΔT, Tv, Zvm) / mol.mass
+                Zvp = compute_Z_vibr(mol, vd, T + ΔT, Tv)
+                Evp = compute_E_vibr(mol, vd, T + ΔT, Tv, Zvp) / mol.mass
 
                 @test c_vibrT <= 0.0
                 @test true == isapprox(c_vibrT, (Evp - Evm) / (2 * ΔT), rtol=rtol)
 
-                Zvm = compute_Z_vibr(mol, T, Tv - ΔT)
-                Evm = compute_E_vibr(mol, T, Tv - ΔT, Zvm) / mol.mass
-                Zvp = compute_Z_vibr(mol, T, Tv + ΔT)
-                Evp = compute_E_vibr(mol, T, Tv + ΔT, Zvp) / mol.mass
+                Zvm = compute_Z_vibr(mol, vd, T, Tv - ΔT)
+                Evm = compute_E_vibr(mol, vd, T, Tv - ΔT, Zvm) / mol.mass
+                Zvp = compute_Z_vibr(mol, vd, T, Tv + ΔT)
+                Evp = compute_E_vibr(mol, vd, T, Tv + ΔT, Zvp) / mol.mass
 
                 @test true == isapprox(c_vibrTv, (Evp - Evm) / (2 * ΔT), rtol=rtol)
             end
@@ -250,8 +231,9 @@ end
 
 
     T_arr = [500.0, 1000.0, 2000.0]
-    for molname in ["N2", "O2", "NO"]
+    for molname in molnames
         mol = create_molecule("data/particles.yaml", molname, anharmonic=true, simplified_anharmonic=true)
+        vd = create_vibrational_distribution(true, true, false)
         for T in T_arr
             atomarr = Atom[]
             natomarr = Float64[]
@@ -263,10 +245,10 @@ end
             Zr = compute_Z_rot(mol, T)
             Er = compute_E_rot(mol, T, Zr)
             crotarr = [compute_c_rot(mol, T, Zr, Er)]
-            Zv = compute_Z_vibr(mol, T, T)
-            Ev = compute_E_vibr(mol, T, T, Zv)
+            Zv = compute_Z_vibr(mol, vd, T, T)
+            Ev = compute_E_vibr(mol, vd, T, T, Zv)
             crotarr = [compute_c_rot(mol, T, Zr, Er)]
-            cvibrarr = [compute_c_vibrTv(mol, T, T, Zv, Ev)]
+            cvibrarr = [compute_c_vibrTv(mol, vd, T, T, Zv, Ev)]
 
             c_v_c_p = compute_c_v_and_c_p(T, atomarr, [mol], natomarr, [1e23], crotarr, cvibrarr)
             @test true == isapprox(c_v_c_p[2], c_v_c_p_gy[2], rtol=rtol)
@@ -281,7 +263,8 @@ end
 
     ΔT = 300
     for molname in ["N2", "O2", "NO"]
-        mol = create_molecule("data/particles.yaml", molname, anharmonic=false)
+        mol = create_molecule("data/particles.yaml", molname, anharmonic=true)
+        vd = create_vibrational_distribution(true, true, false)
         for T in T_arr
 
             atomarr = Atom[]
@@ -291,16 +274,16 @@ end
 
             Zr = compute_Z_rot(mol, T)
             Er_arr = [compute_E_rot(mol, T, Zr)]
-            Zv = compute_Z_vibr(mol, T, T)
-            Ev_arr = [compute_E_vibr(mol, T, T, Zv)]
+            Zv = compute_Z_vibr(mol, vd, T, T)
+            Ev_arr = [compute_E_vibr(mol, vd, T, T, Zv)]
 
             U_h_1 = compute_U_and_h(T, atomarr, [mol], natomarr, [1e23], Er_arr, Ev_arr)
             U_h_1_gy = compute_U_and_h_gupta_yos(T, atomarr, [mol], natomarr, [1e23], Er_arr, Ev_arr)
 
             Zr = compute_Z_rot(mol, T + ΔT)
             Er_arr = [compute_E_rot(mol, T + ΔT, Zr)]
-            Zv = compute_Z_vibr(mol, T + ΔT, T + ΔT)
-            Ev_arr = [compute_E_vibr(mol, T + ΔT, T + ΔT, Zv)]
+            Zv = compute_Z_vibr(mol, vd, T + ΔT, T + ΔT)
+            Ev_arr = [compute_E_vibr(mol, vd, T + ΔT, T + ΔT, Zv)]
 
             U_h_2 = compute_U_and_h(T + ΔT, atomarr, [mol], natomarr, [1e23], Er_arr, Ev_arr)
             U_h_2_gy = compute_U_and_h_gupta_yos(T + ΔT, atomarr, [mol], natomarr, [1e23], Er_arr, Ev_arr)
@@ -318,6 +301,8 @@ end
     ΔT = 0.05
     n = 1e23
 
+    rtol_mix = 0.01
+
     for (atomname, molname) in zip(["N", "O", "N"], ["N2", "O2", "NO"])
         atom = create_atom("data/particles.yaml", atomname)
 
@@ -325,36 +310,38 @@ end
         # for a harmonic oscillator everything's nice though, and we're testing thermal equilibrium here
         mol = create_molecule("data/particles.yaml", molname, anharmonic=false)
 
+        vd = create_vibrational_distribution(false, false, false)
+
+        mixture = create_mixture([atom], [mol], vd)
         for T in T_arr
 
             Zrm = compute_Z_rot(mol, T - ΔT)
             Er_arrm = [compute_E_rot(mol, T - ΔT, Zrm)]
-            Zvm = compute_Z_vibr(mol, T - ΔT, T - ΔT)
-            Ev_arrm = [compute_E_vibr(mol, T - ΔT, T - ΔT, Zvm)]
+            Zvm = compute_Z_vibr(mol, vd, T - ΔT, T - ΔT)
+            Ev_arrm = [compute_E_vibr(mol, vd, T - ΔT, T - ΔT, Zvm)]
 
             Zrp = compute_Z_rot(mol, T + ΔT)
             Er_arrp = [compute_E_rot(mol, T + ΔT, Zrp)]
-            Zvp = compute_Z_vibr(mol, T + ΔT, T + ΔT)
-            Ev_arrp = [compute_E_vibr(mol, T + ΔT, T + ΔT, Zvp)]
+            Zvp = compute_Z_vibr(mol, vd, T + ΔT, T + ΔT)
+            Ev_arrp = [compute_E_vibr(mol, vd, T + ΔT, T + ΔT, Zvp)]
 
             Zr = compute_Z_rot(mol, T)
             Er = compute_E_rot(mol, T, Zr)
-            Zv = compute_Z_vibr(mol, T, T)
-            Ev = compute_E_vibr(mol, T, T, Zv)
+            Zv = compute_Z_vibr(mol, vd, T, T)
+            Ev = compute_E_vibr(mol, vd, T, T, Zv)
 
             crotarr = [compute_c_rot(mol, T, Zr, Er)]
-            cvibrarr = [compute_c_vibrTv(mol, T, T, Zv, Ev)]
+            cvibrarr = [compute_c_vibrTv(mol, vd, T, T, Zv, Ev)]
 
             for x_atom in [0.0, 0.25, 0.5, 1.0]
-
-
-
                 U_h_1 = compute_U_and_h(T - ΔT, [atom], [mol], [n * x_atom], [(1.0 - x_atom) * n], Er_arrm, Ev_arrm)
                 U_h_2 = compute_U_and_h(T + ΔT, [atom], [mol], [n * x_atom], [(1.0 - x_atom) * n], Er_arrp, Ev_arrp)
-
                 
                 c_v_c_p = compute_c_v_and_c_p(T, [atom], [mol], [n * x_atom], [(1.0 - x_atom) * n], crotarr, cvibrarr)
+
+                compute_mixture!(mixture, T - ΔT, [T - ΔT], [n * x_atom], [(1.0 - x_atom) * n])
                 @test true == isapprox(c_v_c_p[1], (U_h_2[1] - U_h_1[1]) / (2 * ΔT), rtol=rtol)
+                @test true == isapprox(mixture.U, U_h_1[1], rtol=rtol_mix)
             end
         end
     end
