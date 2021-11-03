199673b491c4a3ae95ba90d80642f6da57af9f3b
Georgii Oblapenko <kunstmord@kunstmord.com>
2021-11-03 15:28:08 +0100

    Export of more functions

--- src/Prong.jl
+++ src/Prong.jl
@@ -5,5 +5,8 @@ include("mixture.jl")
 
 export Atom, Molecule, VibrationalDistribution
 export create_atom, create_molecule, create_vibrational_distribution, Mixture, create_mixture, compute_mixture!
+export compute_Z_vibr, compute_Z_rot, compute_xi_vibr, compute_E_vibr, compute_E_rot
+export compute_c_vibrT, compute_c_vibrTv, compute_c_vibr_equilibrium, compute_c_rot, compute_c_v_and_c_p_equilibrium_gupta_yos
+export compute_c_v_and_c_p, compute_c_v_and_c_p_equilibrium_gupta_yos, compute_U_and_h_equilibrium_gupta_yos, compute_U_and_h
 
 end
